4d1ab1e43e3b66deaee5e4ca906224909b323c18
Paul Miller <paul@jettero.pl>
2019-03-12 08:08:12 -0400

    grime

--- space/roll.py
+++ space/roll.py
@@ -19,11 +19,33 @@ class ARoll(int):
         return r
 
 class Roller:
-    def __init__(self, num=1, sides=6, bonus=0):
+    unusual = {
+        # Grime Dice (non-transitive cycle dice):
+        # https://youtu.be/6JwEYamjXpA?t=2169
+        # http://www.latkin.org/blog/2015/01/16/non-transitive-grime-dice-via-mathematica/
+        'red':     (4,4,4,4,4,9),
+        'blue':    (2,2,2,7,7,7),
+        'olive':   (0,5,5,5,5,5),
+        'yellow':  (3,3,3,3,8,8),
+        'magenta': (1,1,6,6,6,6),
+    }
+
+    def __init__(self, num=1, sides=6, bonus=0, unusual=None):
+        if unusual is not None:
+            if unusual not in self.unusual:
+                raise TypeError(f'"{unusual}" die is not defined')
+            sides = None
+        self.u = unusual
         self.n = num
         self.d = sides
         self.b = bonus
+
     def roll(self, min_=1, max_=None):
+        if self.u is not None:
+            d = self.b
+            for i in range(0, self.n):
+                d += random.choice(self.faces)
+            return ARoll(d, crit=False, fumb=False, min_=0, max_=None)
         d = self.b
         crit = True
         fumb = True
@@ -40,18 +62,34 @@ class Roller:
         if self.b > 0:   b = f'+{self.b}'
         elif self.b < 0: b = f'{self.b}'
         else:            b = ''
+        if self.u:
+            return f'{self.n}«{self.u}{self.faces}»{b}'
         return f'{self.n}d{self.d}{b}'
 
     @property
+    def faces(self):
+        if self.u is not None:
+            return self.unusual[self.u]
+        return tuple(range(1,self.d+1))
+
+    @property
     def max(self):
+        if self.u is not None:
+            return max(self.faces) * self.n + self.b
         return self.n * self.d + self.b
 
     @property
     def min(self):
+        if self.u is not None:
+            return min(self.faces) * self.n + self.b
         return self.n + self.b
 
     @property
     def mean(self):
+        if self.u:
+            f = self.faces
+            e = sum(f) / len(f)
+            return self.n * e + self.b
         if self.n < 1:
             return self.b
         # o = list(range(1,self.d+1))
@@ -68,10 +106,11 @@ class Roller:
 
 def _lark():
     class MakeRoller(Transformer):
-        def     num(self, v): return {  'num':  int(v[0])}
-        def   sides(self, v): return {'sides':  int(v[0])}
-        def   bonus(self, v): return {'bonus':  int(v[0])}
-        def penalty(self, v): return {'bonus': -int(v[0])}
+        def     num(self, v): return {  'num':   int(v[0])}
+        def   sides(self, v): return {'sides':   int(v[0])}
+        def   bonus(self, v): return {'bonus':   int(v[0])}
+        def penalty(self, v): return {'bonus':  -int(v[0])}
+        def unusual(self, v): return {'unusual': str(v[0])}
 
         def desc(self, v):
             ret = dict()
@@ -85,12 +124,20 @@ def _lark():
                 args.update(i)
             return Roller(**args)
 
-    return Lark('''
+    unusual_names = [ f'"{n}"' for n in Roller.unusual ]
+    unusual_names = ' | '.join(unusual_names)
+
+    # NOTE: we accept "red" and "olive" as names of dice;
+    # and we accept "d4" and "d8" and "d7" as names of dice.
+    # "d red" is not a name of a die — don't try to make it one.
+
+    return Lark(f'''
         %import common (INT, WS)
         %ignore WS
         ?start: roller
         roller: desc
-        desc: num? sides (bonus | penalty)?
+        !unusual: {unusual_names}
+        desc: num? (unusual|sides) (bonus | penalty)?
         num: INT
         sides: "d" INT
         bonus: "+" INT
@@ -114,6 +161,9 @@ class Roll:
             self._roller = _lark(desc)
         except LarkError as e:
             raise RollError() from e
+
+    def __repr__(self):
+        return repr(self._roller)
 
     def roll(self):
         try:
--- t/test_roll.py
+++ t/test_roll.py
@@ -16,9 +16,62 @@ def test_roll():
         if x == 3: assert x.fumb is True
         if x.fumb: assert x == 3
 
+def _test_mean_trials(r, rel=0.1, trials=50000):
+    bfm = sum([ r.roll() for _ in range(trials) ]) / trials
+    assert pytest.approx(r.mean, rel=rel) == bfm
+
 def test_min_max_mean():
     r = Roll('8d20+7')
-    bfm = sum([ r.roll() for _ in range(50000) ]) / 50000
     assert r.min == 8 + 7
     assert r.max == 8*20 + 7
-    assert pytest.approx(r.mean, rel=0.1) == bfm
+    _test_mean_trials(r, rel=0.1)
+
+def test_1d82():
+    _test_mean_trials(Roll('1d8+2'))
+
+def test_1red7():
+    _test_mean_trials(Roll('1 red +7'))
+
+def test_5olive():
+    _test_mean_trials(Roll('1 olive'))
+
+def _test_wins(l,r, trials=50000):
+    L = Roll(l)
+    R = Roll(r)
+    l,r = 0,0
+    for _ in range(trials):
+        if L.roll() > R.roll(): l += 1
+        else:                   r += 1
+    assert l > r
+
+def test_grime_first_cycle():
+    ''' if highest wins, …
+      red beats blue
+      blue beats olive
+      olive beats yellow
+      yellow beats magenta
+      magenta beats red
+      (wtf??)
+    '''
+
+    _test_wins( 'red',     'blue',    )
+    _test_wins( 'blue',    'olive',   )
+    _test_wins( 'olive',   'yellow',  )
+    _test_wins( 'yellow',  'magenta', )
+    _test_wins( 'magenta', 'red',     )
+
+def test_grime_second_cycle():
+    ''' if highest wins, …
+      2 red loses to 2 blue
+      2 blue loses to 2 olive
+      2 olive loses to 2 yellow
+      2 yellow loses to 2 magenta
+      2 magenta loses to 2 red
+      (wtf??)
+    '''
+
+    _test_wins( '2 blue',    '2 red',     )
+    _test_wins( '2 olive',   '2 blue',    )
+    _test_wins( '2 yellow',  '2 olive',   )
+    _test_wins( '2 magenta', '2 yellow',  )
+    _test_wins( '2 red',     '2 magenta', )
