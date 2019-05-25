8ecbe2b0894aa8b3452aba7c84e1788005aee688
Paul Miller <paul@jettero.pl>
2019-05-25 08:00:46 -0400

    test map moves

---
+++ t/test_map_moves.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+def test_move_me_to_ubi(a_map,objs):
+    assert a_map[objs.me] is not a_map[objs.ubi]
+    a_map[ a_map[objs.ubi].pos ] = objs.me
+    assert a_map[objs.me] is a_map[objs.ubi]
+
+def test_move_ubi_to_me(a_map,objs):
+    assert a_map[objs.me] is not a_map[objs.ubi]
+    a_map[ a_map[objs.me].pos ] = objs.ubi
+    assert a_map[objs.me] is a_map[objs.ubi]
