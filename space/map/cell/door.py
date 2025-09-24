fdcd3ce6c3b1fdd7ecd5d470174cc9b8108fac60
Paul Miller <paul@jettero.pl>
2025-09-24 17:56:58 -0400

    I guess this makes sense.

--- space/map/cell/door.py
+++ space/map/cell/door.py
@@ -1,14 +1,11 @@
 # coding: utf-8
 
 from .cell import Cell
+from ...door import Door as BaseDoor
 
-class Door(Cell):
-    a = '+'
-
-    def __init__(self, *items, mobj=None, pos=None, open=True):
-        super().__init__(*items, mobj=mobj, pos=pos)
-        self.open = open
-
-    @property
-    def abbr(self):
-        return '/' if self.open else '+'
+class Door(BaseDoor, Cell):
+    # Cell already implements container behavior for map tiles; BaseDoor adds
+    # open/locked/stuck semantics and abbr.
+    def __init__(self, *items, mobj=None, pos=None, open=True, locked=False, stuck=False):
+        Cell.__init__(self, *items, mobj=mobj, pos=pos)
+        BaseDoor.__init__(self, open=open, locked=locked, stuck=stuck)
