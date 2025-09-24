8b6e125edda9ac94fedc490cdb25886f5324218b
Paul Miller <paul@jettero.pl>
2025-09-24 09:58:11 -0400

    goom

--- space/map/cell/door.py
+++ space/map/cell/door.py
@@ -4,3 +4,11 @@ from .cell import Cell
 
 class Door(Cell):
     a = '+'
+
+    def __init__(self, *items, mobj=None, pos=None, open=True):
+        super().__init__(*items, mobj=mobj, pos=pos)
+        self.open = open
+
+    @property
+    def abbr(self):
+        return '/' if self.open else '+'
