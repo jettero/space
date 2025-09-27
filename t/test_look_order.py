ac3576362cf52410cfb7a3e6eba3d168c75bc2ab
Paul Miller <paul@jettero.pl>
2025-09-27 14:46:55 -0400

    fixed various other things so the whole test suite passes. We have a lot to do to make all this shit work though

--- t/test_look_order.py
+++ t/test_look_order.py
@@ -1,7 +1,10 @@
 # coding: utf-8
 
 def test_look_shows_living_over_door(a_map, objs):
-    objs.me.do("open door; s")
+    # space/parser.py +260 marks us active when we do something
+    objs.me.do("open door; s; look")
+    # space/parser.py +263 marks us in-active when we're done
+    objs.me.active = True # so we fake it like this
 
     # Render the visible map and ensure the living '@' appears
     sub = a_map.visicalc_submap(objs.me)
