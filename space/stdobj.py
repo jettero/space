9939fa6baf542ff389759c699c6521d1019c3551
Paul Miller <paul@jettero.pl>
2025-10-06 12:20:31 -0400

    return sorta the right thing for a human

--- space/stdobj.py
+++ space/stdobj.py
@@ -8,3 +8,9 @@ from .named import Named
 class StdObj(Named, Containable):
     def __init__(self, **kw):
         super().__init__(**kw)  # MRO insures we call Named.__init__ and Containable.__init__
+
+    def __str__(self):
+        return self.long
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}<{self.short}>'
