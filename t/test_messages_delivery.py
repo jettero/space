73e438a1bb72794f91c79892999de55c713c5e8a
Paul Miller <paul@jettero.pl>
2025-10-06 22:56:32 -0400

    this should work, so annoying

--- t/test_messages_delivery.py
+++ t/test_messages_delivery.py
@@ -1,7 +1,18 @@
 # coding: utf-8
 
 from space.living.msg import ReceivesMessages
+from space.shell.list import Shell as ListShell
 
 def test_simple_and_targeted_delivery(a_map, objs):
-    objs.me.simple_action("$N $vwave.", range=3)
-    objs.me.targeted_action("$N $vattack $t.", objs.stupid, range=6)
+    objs.me.shell = ListShell()
+    objs.dig_dug.shell = ListShell()
+
+    objs.door.open = True
+    objs.me.do_look()
+    assert any(objs.door.abbr in x for x in objs.me.shell.msgs)
+
+    objs.me.simple_action("$N $vwave.")
+    assert len(objs.me.shell.msgs) == 3
+    assert len(objs.dig_dug.shell.msgs) == 1
+    assert objs.me.shell.msgs[-1] == "You wave."
+    assert objs.dig_dug.shell.msgs[-1] == "Paul waves."
