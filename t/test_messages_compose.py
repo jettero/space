35b4dfcfedcf0859b3fb910def89ecfc817401c3
Paul Miller <paul@jettero.pl>
2025-10-08 15:29:32 -0400

    the AI went a wrong direction and I had to clean this up

--- t/test_messages_compose.py
+++ t/test_messages_compose.py
@@ -1,27 +1,29 @@
 # coding: utf-8
 
-from space.living.msg import ReceivesMessages as MessagesMixin
+import pytest
+from space.living.msg import Actors
 
-
-def test_compose_basic_pronouns_and_verb(a_map, objs):
-    m = MessagesMixin()
-    who = [objs.me, objs.stupid]
-    # $N $vattack $t.
-    msg = m.compose(objs.me, "$N $vattack $t.", who)
+def test_compose_basic_pronouns_and_verb(objs):
+    msg = objs.me.compose(objs.me, "$N $vattack $t.", [objs.me, objs.stupid])
     assert isinstance(msg, str)
-    assert msg == "You attack Stupid."
+    assert msg == f"You attack {objs.stupid.a_short}."
 
-    others = m.compose(None, "$N $vattack $t.", who)
+    others = objs.me.compose(None, "$N $vattack $t.", [objs.me, objs.stupid])
     assert others == f"{objs.me.a_short} attacks {objs.stupid.a_short}."
 
 
 def test_compose_objects_variants(a_map, objs):
-    from space.living.msg import ReceivesMessages as MessagesMixin
-
-    m = MessagesMixin()
     who = [objs.me, objs.stupid]
     obs = [objs.ubi]
-    msg_t = m.compose(objs.me, "$N $vlook at $o.", who, *obs)
+    msg_t = objs.me.compose(objs.me, "$N $vlook at $o.", who, *obs)
     assert msg_t == f"You look at {objs.ubi.a_short}."
-    msg_t = m.compose(None, "$N $vlook at $o.", who, *obs)
+    msg_t = objs.me.compose(None, "$N $vlook at $o.", who, *obs)
     assert msg_t == f"{objs.me.a_short} looks at {objs.ubi.a_short}."
+
+
+def test_compose_vare_agreement(objs):
+    msg = objs.me.action(Actors(objs.me, objs.stupid), "$N $vare very cool.")
+
+    assert msg.us == "You are very cool."
+    assert msg.them == "Paul is very cool."
+    assert msg.other == "Paul is very cool."
