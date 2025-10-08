7d9a39467c8b4466df6f7a2b037631fb5607670b
Paul Miller <paul@jettero.pl>
2025-10-08 14:58:19 -0400

    this seems ok so far

--- lrun-shell.py
+++ lrun-shell.py
@@ -4,13 +4,8 @@
 
 import sys
 
-from space.map.room import Room
-from space.shell import ReadlineShell
-from space.living import Human
-from space.map.generate import generate as generate_map
-from space.item import Ubi
-
 from t.troom import a_map, o
+from space.master import MasterControlProgram as MCP
 
 c = sys.argv[1:]
 if c[0:2] in (["get", "bauble"], ["bauble"], ["ubi"]):
@@ -18,4 +13,4 @@ if c[0:2] in (["get", "bauble"], ["bauble"], ["ubi"]):
 else:
     c = ["look"]
 
-ReadlineShell(owner=o.me, init=c).loop()
+MCP().start_instance(type="local", username="jettero", map=a_map, body=o.me, init=c)
---
+++ space/master.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# WE ARE THE MASTER CONTROL PROGRAM
+
+from space.living.msg import HasShell
+from space.shell import ReadlineShell
+
+class MasterControlProgram:
+    """Minimal MCP for local instances.
+
+    For now, supports starting a local interactive shell instance by wiring a
+    provided body (owner) and optional prebuilt map/objects. This mirrors what
+    lrun-shell.py currently does but centralizes the boot logic here so future
+    transports (ssh/telnet) can call the same entry point.
+    """
+
+    def start_instance(self, type="local", username=None, map=None, body=None, init=None):  # pylint: disable=redefined-builtin
+        """Start an instance.
+
+        Args:
+            type: instance type; only 'local' supported for now.
+            username: informational; unused for now.
+            map: optional map to render on look; can be None.
+            body: the Living to own the shell; required.
+            init: list/tuple of initial commands.
+        """
+        if type != "local":
+            raise NotImplementedError("only local instances supported")
+        if not isinstance(body, HasShell):
+            raise ValueError("body is required to start instance")
+
+        shell = ReadlineShell(owner=body, init=init)
+        shell.loop()
