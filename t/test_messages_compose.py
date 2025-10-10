e052a124a378a936ea34738ff108ab7159867416
Paul Miller <paul@jettero.pl>
2025-10-10 10:01:36 -0400

    add back these parameterized tests

--- t/test_messages_compose.py
+++ t/test_messages_compose.py
@@ -1,14 +1,14 @@
 # coding: utf-8
 
 import pytest
-from space.living.msg import Actors, Messages
+from space.living.msg import Messages
 
 
-def _compose(me, targ, msg):
+def _compose(me, targ, msg, *obs):
     return Messages(
-        me.compose(me, msg, [me, targ]),
-        me.compose(targ, msg, [me, targ]),
-        me.compose(None, msg, [me, targ]),
+        me.compose(me, msg, [me, targ], *obs),
+        me.compose(targ, msg, [me, targ], *obs),
+        me.compose(None, msg, [me, targ], *obs),
     )
 
 
@@ -23,34 +23,24 @@ def test_compose_basic_pronouns_and_verb(objs):
     assert msg.them == f"{objs.me.a_short} attacks you."
     assert msg.other == f"{objs.me.a_short} attacks {objs.stupid.a_short}."
 
+@pytest.mark.parametrize(
+    "msgs",
+    [
+        ("$N $vdie.", "You die.", "Paul dies."),
+        ("$N $vare very cool.", "You are very cool.", "Paul is very cool."),
+        ("$N $vhave $o.", "You have a bauble.", "Paul has a bauble."),
+        ('$N $vdo "things" to $o.', 'You do "things" to a bauble.', 'Paul does "things" to a bauble.'),
+    ],
+)
+def test_action_verb_agreement(objs, msgs):
+    try:
+        to_parse, us, them, other = msgs
+    except ValueError:
+        to_parse, us, them = msgs
+        other = them
 
-# def test_compose_objects_variants(a_map, objs):
-#     who = [objs.me, objs.stupid]
-#     obs = [objs.ubi]
-#     msg_t = objs.me.compose(objs.me, "$N $vlook at $o.", who, *obs)
-#     assert msg_t == f"You look at {objs.ubi.a_short}."
-#     msg_t = objs.me.compose(None, "$N $vlook at $o.", who, *obs)
-#     assert msg_t == f"{objs.me.a_short} looks at {objs.ubi.a_short}."
+    msg = _compose(objs.me, objs.stupid, to_parse, objs.ubi)
 
-
-# @pytest.mark.parametrize(
-#     "msgs",
-#     [
-#         ("$N $vdie.", "You die.", "Paul dies."),
-#         ("$N $vare very cool.", "You are very cool.", "Paul is very cool."),
-#         ("$N $vhave $o.", "You have a bauble.", "Paul has a bauble."),
-#         ('$N $vdo "things" to $o.', 'You do "things" to a bauble.', 'Paul does "things" to a bauble.'),
-#     ],
-# )
-# def test_action_verb_agreement(objs, msgs):
-#     try:
-#         to_parse, us, them, other = msgs
-#     except ValueError:
-#         to_parse, us, them = msgs
-#         other = them
-
-#     msg = objs.me.action(Actors(objs.me, objs.stupid), to_parse, objs.ubi)
-
-#     assert msg.us == us
-#     assert msg.them == them
-#     assert msg.them == other
+    assert msg.us == us
+    assert msg.them == them
+    assert msg.them == other
