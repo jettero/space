d5fe4e77d571301ae57439b633006581799e97c4
Paul Miller <paul@jettero.pl>
2025-10-17 11:32:56 -0400

    it's always kinda bugged me how verb/__init__.py was doing actual work instead of just importing

---
+++ space/verb/gen.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+from ..find import find_by_classname
+
+def find_action_classes():
+    return find_by_classname("space.verb", "Action")
+
+
+def load_verbs():
+    if os.environ.get("SPACE_DISABLE_EMOTE_REGISTRY", False):
+        return [c() for c in find_action_classes()]
+
+    from .emote import load_emotes
+
+    return [c() for c in find_action_classes()] + load_emotes()
+
+VERBS = {v.name: v for v in load_verbs()}
+VERBS.update({n:v for v in VERBS.values() for n in v.nick if n not in VERBS})
+
+def find_verb(x):
+    if x and isinstance(x, str):
+        x = x.lower()
+        if v := VERBS.get(x): # exact matching via get()
+            return {v}
+        return {v for n, v in VERBS.items() if v.match(x)}
+    return set()
