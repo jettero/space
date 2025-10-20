d98c658bb29ef160b34a7116f95bd3fcc83f9409
Paul Miller <paul@jettero.pl>
2025-10-20 10:46:47 -0400

    pretty major bug here

--- space/router.py
+++ space/router.py
@@ -18,8 +18,7 @@ class MethodRouter:
         self.obj = obj if isinstance(obj, weakref.ProxyTypes) else weakref.proxy(obj)
         self.callback = callback
         self.dne_ok = dne_ok
-        self.dir = tuple(m for m in dir(obj) if m.startswith(top))
-        log.debug("[MethodRouter] top=%s dir=%s", top, self.dir)
+        self.dir = tuple(m for m in dir(obj) if m == top or m.startswith(f'{top}_'))
 
     def __iter__(self):
         yield from self.dir
