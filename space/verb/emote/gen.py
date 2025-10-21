7521417a0d3ccf8c21d8e09a4da906980000363a
Paul Miller <paul@jettero.pl>
2025-10-21 19:46:22 -0400

    black

--- space/verb/emote/gen.py
+++ space/verb/emote/gen.py
@@ -25,16 +25,17 @@ BLESSED_TOKENS = {
 }
 
 SAFE_TOKEN = re.compile(r"^[a-zA-Z][A-Za-z0-9-]+$")
-EmoteInfo = namedtuple("EmoteInfo", 'sig template vars args wrd_vals'.split())
+EmoteInfo = namedtuple("EmoteInfo", "sig template vars args wrd_vals".split())
+
 
 def compute_db(name, patterns):
     ret = dict()
 
-    xlate_templates = {sig:template for sig,template in patterns.items()}
+    xlate_templates = {sig: template for sig, template in patterns.items()}
     for sig, template in patterns.items():
         if isinstance(template, list):
-            template = [ (xlate_templates[item[1:]] if item.startswith('=') else item) for item in template ]
-        elif template.startswith('='):
+            template = [(xlate_templates[item[1:]] if item.startswith("=") else item) for item in template]
+        elif template.startswith("="):
             template = xlate_templates[template[1:]]
         tokens = [x for x in sig.split(" ") if x]
         fn_name = [name.replace("-", "_")]
@@ -65,9 +66,10 @@ def compute_db(name, patterns):
         fn_name = "_".join(fn_name)
         if fn_name not in ret:
             ret[fn_name] = EmoteInfo(sig=sig, template=template, vars=vars, args=args, wrd_vals=wrd_vals)
-        for aname,wv in wrd_vals.items():
+        for aname, wv in wrd_vals.items():
             ret[fn_name].wrd_vals[aname].update(wv)
     return ret
+
 
 class Emote(Verb):
     def __init__(self, name, patterns):
@@ -75,30 +77,30 @@ class Emote(Verb):
             raise ValueError(f'"{name}" is not a good name for an emote')
         self.name = name
         self.rule_db = compute_db(name, patterns)
-        #for fn_name, rule_db_ent in self.rule_db.items():
-            #self.generate_can_fn(fn_name, rule_db_ent)
+        # for fn_name, rule_db_ent in self.rule_db.items():
+        # self.generate_can_fn(fn_name, rule_db_ent)
         super().__init__()
 
     def generate_can_fn(self, fn_name, ent, on_cls=Living, src_only=False):
         fn_name = f"can_{fn_name}"
         fn_args = ", ".join(ent.args)
-        fn_vars = ", ".join( f'"{x}":{x}' for x in ent.vars )
+        fn_vars = ", ".join(f'"{x}":{x}' for x in ent.vars)
 
-        source_code = [ f"def {fn_name}(self, {fn_args}):" ]
+        source_code = [f"def {fn_name}(self, {fn_args}):"]
 
         if ent.wrd_vals:
-            for wrd,vals in ent.wrd_vals.items():
+            for wrd, vals in ent.wrd_vals.items():
                 tv = tuple(vals)
                 source_code.append(f"  if {wrd} not in {tv!r}:")
                 source_code.append(f"    return {{'error': f'\"{{{wrd}}}\" not understood'}}")
-            source_code.append(f"  template = ent.word_vals[\"{wrd}\"][{wrd}]")
-        elif isinstance(ent.template, (tuple,list)):
-            source_code.append(f'  template = random.choice({ent.template!r})')
+            source_code.append(f'  template = ent.word_vals["{wrd}"][{wrd}]')
+        elif isinstance(ent.template, (tuple, list)):
+            source_code.append(f"  template = random.choice({ent.template!r})")
         else:
-            source_code.append(f'  template = {ent.template!r}')
+            source_code.append(f"  template = {ent.template!r}")
 
         source_code.append(f'  return True, {{ {fn_vars}, "template":template }}')
-        source_code = "\n".join(source_code) + '\n'
+        source_code = "\n".join(source_code) + "\n"
 
         if src_only:
             return source_code
