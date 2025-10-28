055f60020d23f01f343ea8bb08bf8e7f403ed18a
Paul Miller <paul@jettero.pl>
2025-10-28 13:26:29 -0400

    black

--- t/test_synthetic_verb.py
+++ t/test_synthetic_verb.py
@@ -17,43 +17,64 @@ class Action(Verb):
 
 register_outside_verb(Action())
 
+
 def can_itsatest(actor):
     MARKERS.append(("can",))
     return True, {}
+
 
 def can_itsatest_words(actor, words: tuple[str, ...]):
     MARKERS.append(("can", "words", words))
     return True, {"words": words}
 
+
 def can_itsatest_obj(actor, obj: StdObj):
     MARKERS.append(("can", "obj", obj))
     return True, {"obj": obj}
+
 
 def can_itsatest_obj_words(actor, obj: StdObj, words: tuple[str, ...]):
     MARKERS.append(("can", "obj", obj, "words", words))
     return True, {"obj": obj, "words": words}
 
+
+def can_itsatest_living(actor, living: Living):
+    MARKERS.append(("can", "living", living))
+    return True, {"target": living}
+
+
 Living.can_itsatest = can_itsatest
 Living.can_itsatest_words = can_itsatest_words
 Living.can_itsatest_obj = can_itsatest_obj
 Living.can_itsatest_obj_words = can_itsatest_obj_words
+Living.can_itsatest_living = can_itsatest_living
+
 
 def do_itsatest(actor):
     MARKERS.append(("do",))
 
+
 def do_itsatest_words(actor, words):
     MARKERS.append(("do", "words", words))
+
 
 def do_itsatest_obj(actor, obj):
     MARKERS.append(("do", "obj", obj))
 
+
 def do_itsatest_obj_words(actor, obj, words):
     MARKERS.append(("do", "obj", obj, "words", words))
+
+
+def do_itsatest_living(actor, target):
+    MARKERS.append(("do", "living", target))
+
 
 Living.do_itsatest = do_itsatest
 Living.do_itsatest_words = do_itsatest_words
 Living.do_itsatest_obj = do_itsatest_obj
 Living.do_itsatest_obj_words = do_itsatest_obj_words
+Living.do_itsatest_living = do_itsatest_living
 
 
 def test_synthetic_verb_me_do(me):
@@ -82,3 +103,10 @@ def test_synthetic_verb_env_variants(me, objs):
     assert me.do("itsatest foo bar") is True
     assert ("can", "words", ("foo", "bar")) in MARKERS
     assert [m for m in MARKERS if m and m[0] == "do"] == [("do", "words", ("foo", "bar"))]
+
+
+def test_synthetic_verb_living(me, objs):
+    MARKERS.clear()
+    assert me.do("itsatest stupid") is True
+    assert any(m[:2] == ("can", "living") for m in MARKERS)
+    assert [m for m in MARKERS if m and m[0] == "do" and m[1] == "living"]
