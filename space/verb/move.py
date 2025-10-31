fa0d62c10c52c4b728e93b0d4fe57b02a68e13dc
Paul Miller <paul@jettero.pl>
2025-10-31 10:34:52 -0400

    be more careful about this

--- space/verb/move.py
+++ space/verb/move.py
@@ -13,12 +13,12 @@ class Action(Verb):
     nick = ["mv"]
 
     def preprocess_tokens(self, me, **tokens):
-        # log.debug("super(%s).preprocess_tokens(%s)", self, tokens)
         tokens = super().preprocess_tokens(me, **tokens)
-        # log.debug("%s.preprocess_tokens(%s)", self, tokens)
-        if "moves" in tokens:
-            moves = " ".join(tokens.pop("moves"))
-            if is_direction_string(moves):
-                tokens["moves"] = tuple(move_string_to_dirs(moves))
-            log.debug(" %s -> %s", moves, tokens.get("moves"))
+
+        if moves := tokens.get("moves"):
+            if isinstance(moves, tuple) and all(isinstance(x, str) for x in moves):
+                moves = " ".join(moves)
+                if is_direction_string(moves):
+                    tokens["moves"] = moves2 = tuple(move_string_to_dirs(moves))
+                    log.debug("preprocess_tokens(%s) -> %s", repr(moves), repr(moves2))
         return tokens
