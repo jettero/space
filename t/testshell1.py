5d247f018822777f3720d22c3603e424b4ab10d1
Paul Miller <paul@jettero.pl>
2025-11-05 11:39:03 -0500

    we may make more test shells later

---
+++ t/testshell1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+# pylint: disable=invalid-name,unused-import,no-member
+
+import sys
+
+from t.troom import a_map, o
+from space.master import MasterControlProgram as MCP
+
+c = sys.argv[1:]
+if c[0:2] in (["get", "bauble"], ["bauble"], ["ubi"]):
+    c = ["open door; sSW6s2w; get bauble"]
+else:
+    c = ["look"]
+
+MCP().start_instance(type="local", username="jettero", map=a_map, body=o.me, init=c)
