7ab59df66db09537ca3096408942094a511be6aa
Paul Miller <paul@jettero.pl>
2025-11-06 15:50:11 -0500

    this crashes, good

--- t/test_interactive_shell.py
+++ t/test_interactive_shell.py
@@ -5,3 +5,10 @@ def test_shell_basic(shell_proc):
     shell_proc.sendline("look")
     ok, _ = shell_proc.expect(r"/space/ ", timeout=1)
     assert ok
+
+
+def test_shell_no_double_prompt(shell_proc):
+    shell_proc.expect(r"/space/ ", timeout=2)
+    ok, data = shell_proc.expect("Application is already running.", timeout=10)
+    assert not ok, data
+    assert "Application is already running." not in shell_proc.captured
