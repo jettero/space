14d3c34d86380ab190b999e435f9eb0efd8f182a
Paul Miller <paul@jettero.pl>
2025-11-07 12:46:36 -0500

    another test to show the current menu problem

--- t/test_interactive_shell.py
+++ t/test_interactive_shell.py
@@ -31,15 +31,31 @@ def test_shell_completion_float_position(shell_proc):
     message = f"row={row} col={col} capbuf=<<{shell_proc.captured[-200:]!r}>>"
 
     # verify the completion menu is above lines[23]
-    menu_lines = [
-        i
-        for i, line in enumerate(lines)
-        if "Emote" in line or "Verb" in line
-    ]
+    menu_lines = [i for i, line in enumerate(lines) if "Emote" in line or "Verb" in line]
     assert menu_lines, message
     assert all(x < 23 for x in menu_lines), message
 
     # but now also verify the prompt is still on the last line of the terminal
+    assert row == 24, message
+    assert "/space/" in lines[-1], message
+
+
+def test_shell_menu_destruct_prompt_position(shell_proc):
+    # make sure we start out aligned to the bottom of the screen
+    for i in range(5):
+        shell_proc.sendline("look")
+        shell_proc.expect(r"/space/ ", timeout=1)
+    lines, row, col = shell_proc.terminal_state(width=80, height=25)
+    message = f"row={row} col={col} capbuf=<<{shell_proc.captured[-200:]!r}>>"
+    assert row == 24, message
+    assert "/space/" in lines[-1], message
+
+    shell_proc.send("l\t\t")
+    shell_proc.expect(r"/space/ ", timeout=1)
+    shell_proc.send("\x08")
+
+    lines, row, col = shell_proc.terminal_state(width=80, height=25)
+    message = f"row={row} col={col} capbuf=<<{shell_proc.captured[-200:]!r}>>"
     assert row == 24, message
     assert "/space/" in lines[-1], message
 
