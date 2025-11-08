517621e9d9d2250f89c635803e3946d4c640cb5a
Paul Miller <paul@jettero.pl>
2025-11-08 11:26:43 -0500

    autorun a lotta say

--- lrun-shell.py
+++ lrun-shell.py
@@ -10,6 +10,9 @@ from space.master import MasterControlProgram as MCP
 c = sys.argv[1:]
 if c[0:2] in (["get", "bauble"], ["bauble"], ["ubi"]):
     c = ["open door; sSW6s2w; get bauble"]
+elif c[0:2] in (['lotsa', 'say'], ['say', 'alot']):
+    c = [f'say hiya{x}' for x in range(30)]
+    # c.append("/quit")
 else:
     c = ["look"]
 
