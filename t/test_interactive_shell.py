2fab59b912cc01683f964642cd13ae002f6bb113
Paul Miller <paul@jettero.pl>
2025-11-08 14:23:14 -0500

    fuck

--- t/test_interactive_shell.py
+++ t/test_interactive_shell.py
@@ -14,10 +14,11 @@ def test_shell_basic(shell_proc):
 def test_say_a_lot(shell_proc):
     for x in range(80):
         shell_proc.sendline(f"say hiya{x}")
-        pat = f"(?i:Hiya{x})"
+        pat = f"Hiya{x}\\."
         ok, _ = shell_proc.expect(pat, timeout=1)
         lines, row, col = shell_proc.terminal_state(width=80, height=25)
         message = f"row={row} col={col} pat={pat} capbuf=<<{shell_proc.captured[-200:]!r}>>"
+        assert False, message
         assert ok, message
 
 
