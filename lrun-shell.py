5b46dc8b0270045d3952684f51b57de25b0844ac
Paul Miller <paul@jettero.pl>
2025-11-10 16:44:35 -0500

    profile

--- lrun-shell.py
+++ lrun-shell.py
@@ -2,10 +2,12 @@
 # coding: utf-8
 # pylint: disable=invalid-name,unused-import,no-member
 
-import sys
+import sys, cProfile, pstats
 
 from t.troom import a_map, o
 from space.master import MasterControlProgram as MCP
+
+profile = False
 
 c = sys.argv[1:]
 if c[0:2] in (["get"], ["ubi"], ["get", "bauble"], ["bauble"], ["ubi"]):
@@ -17,7 +19,17 @@ elif c[0:1] in (["fill"],):
     c = ["open door; sSW6s2w; get bauble; l; l; l; smile; grin; bow", *(
         f'say this is a thing I said (x={x})' for x in range(200)
         )]
+elif c[0:1] in (["profile"],):
+    c = [ f'say this is a thing I said (x={x})' for x in range(200) ]
+    c.append('/quit')
+    profile = True
 else:
     c = ["look"]
 
-MCP().start_instance(type="local", username="jettero", map=a_map, body=o.me, init=c)
+if profile:
+    with cProfile.Profile() as pr:
+        MCP().start_instance(type="local", username="jettero", map=a_map, body=o.me, init=c)
+    with open('profile.txt', 'w') as f:
+        pstats.Stats(pr, stream=f).sort_stats('cumulative').print_stats()
+else:
+    MCP().start_instance(type="local", username="jettero", map=a_map, body=o.me, init=c)
