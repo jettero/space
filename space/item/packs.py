1b2f028edae14ed6f32ba03749bcf44788f68cd2
Paul Miller <paul@jettero.pl>
2025-11-16 17:02:04 -0500

    Container isn't enough

--- space/item/packs.py
+++ space/item/packs.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 
 from space.container import Container
+from space.stdobj import StdObj
 
 
-class Sack(Container):
+class Sack(Container, StdObj):
     mass = 0.1
     volume = 0.25
     mass_capacity = 5
