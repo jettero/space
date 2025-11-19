5daafedbd923d0f90b4d83d947d30f6af323fa40
Paul Miller <paul@jettero.pl>
2025-11-19 15:21:42 -0500

    this note is outdated and we also fixed that

--- space/living/gender.py
+++ space/living/gender.py
@@ -2,11 +2,8 @@
 
 from ..named import Named
 
-# XXX: what about females with a dick? this module is very 2015 I have
-# literally no idea how to fix this. Mechanically, the mudlib is only concerned
-# with 'sex', if it's concerned with it at all. For gender, we just leave the
-# pronouns super flexible for players that need this and pretend gender is a
-# reproductive modality?
+# I tried to make this as inclusive as possible, but I think it doesn't matter
+# very much if people can simply change it to what they need to express.
 
 
 class Gender(Named):
