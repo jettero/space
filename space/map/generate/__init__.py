fa9a631869b6a5cac5a10a68cc8ed8784c9ad6bd
Paul Miller <paul@jettero.pl>
2025-12-19 17:16:40 -0500

    unfuck-up the generate/__init__.py

--- space/map/generate/__init__.py
+++ space/map/generate/__init__.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 
-from . import tools, toroid
-
-__all__ = ["tools", "toroid"]
+from .rdropper import generate_rooms
+from .base import sparse
+from .rdc import generate
+from .boxedin import generate as boxed_in
+from .lumpy import generate as lumpy_room
+from .toroid import generate_station as toroid_station
