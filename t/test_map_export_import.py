b803039661fb5fdb2427cfa9f7bf222822dfb272
Paul Miller <paul@jettero.pl>
2025-12-19 17:05:02 -0500

    ws

--- t/test_map_export_import.py
+++ t/test_map_export_import.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from t.troom import gen_troom
 from space.map import export_map_to_path, import_map_from_path
 
+
 @pytest.fixture
 def tmap_file():
     Path("t/tmp").mkdir(exist_ok=True)
